664690b1c036a4bcc84ca83c6ebad2e4c9d1e792
Jon-007-42 <38316625+Jon-007-42@users.noreply.github.com>
2025-06-13 18:03:00 +0200

    Create main.ino
    feat: add main.ino placeholder

---
+++ firmware/main.ino
@@ -0,0 +1,13 @@
+/* 3DGuard – Arduino Mega
+   TODO: læs sensorer og send over Serial
+*/
+
+void setup() {
+  Serial.begin(115200);
+  // pinMode-opsætning kommer senere
+}
+
+void loop() {
+  // fx Serial.println("Heartbeat");
+  delay(1000);
+}
